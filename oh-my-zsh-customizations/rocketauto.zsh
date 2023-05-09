66d11b162dcde5e123aa529566ad998dae61c8c0
Andy Pickler <andypickler@rocketauto.com>
2023-05-09 12:07:05 -0600

    Update customizations

--- oh-my-zsh-customizations/rocketauto.zsh
+++ oh-my-zsh-customizations/rocketauto.zsh
@@ -1,3 +1,8 @@
-# exports
-export NODE_EXTRA_CA_CERTS=/Users/APickler/qlcerts.pem
 export APOLLO_TELEMETRY_DISABLED=1
+export AWS_SHARED_CREDENTIALS_FILE=/Users/APickler/Downloads/credentials
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+
+if [[ -f /etc/ssl/certs/ql-ca-bundle-openssl.crt ]]; then
+  export AWS_CA_BUNDLE=/etc/ssl/certs/ql-ca-bundle-openssl.crt
+  export NODE_EXTRA_CA_CERTS=/etc/ssl/certs/ql-ca-bundle-openssl.crt
+fi
