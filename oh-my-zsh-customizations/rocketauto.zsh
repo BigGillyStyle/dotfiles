393925313c93d37fa1c24790df71af64251332b9
Andy Pickler <andypickler@rocketauto.com>
2023-05-10 09:49:47 -0600

    Add Amplify CLI to PATH

--- oh-my-zsh-customizations/rocketauto.zsh
+++ oh-my-zsh-customizations/rocketauto.zsh
@@ -1,6 +1,7 @@
 export APOLLO_TELEMETRY_DISABLED=1
 export AWS_SHARED_CREDENTIALS_FILE=/Users/APickler/Downloads/credentials
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+export PATH="$HOME/.amplify/bin:$PATH"
 
 if [[ -f /etc/ssl/certs/ql-ca-bundle-openssl.crt ]]; then
   export AWS_CA_BUNDLE=/etc/ssl/certs/ql-ca-bundle-openssl.crt
