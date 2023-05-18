7161636265442620c2af74dff42c288695921260
Andy Pickler <andypickler@rocketauto.com>
2023-05-18 14:09:47 -0600

    Add openssl.cnf env var

--- oh-my-zsh-customizations/rocketauto.zsh
+++ oh-my-zsh-customizations/rocketauto.zsh
@@ -2,6 +2,7 @@ export APOLLO_TELEMETRY_DISABLED=1
 export AWS_SHARED_CREDENTIALS_FILE=/Users/APickler/Downloads/credentials
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
 export PATH="$HOME/.amplify/bin:$PATH"
+export OPENSSL_CONF="$HOME/openssl.cnf"
 
 if [[ -f /etc/ssl/certs/ql-ca-bundle-openssl.crt ]]; then
   export AWS_CA_BUNDLE=/etc/ssl/certs/ql-ca-bundle-openssl.crt
