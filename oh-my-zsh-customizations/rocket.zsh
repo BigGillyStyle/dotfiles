8a8be61decc6edfedf03b07c78fd7fbf6092844f
Andy Pickler <andy.pickler@gmail.com>
2024-10-08 06:50:37 -0600

    Add RKT cert for Node

--- oh-my-zsh-customizations/rocket.zsh
+++ oh-my-zsh-customizations/rocket.zsh
@@ -3,6 +3,7 @@
 # exports
 export APOLLO_TELEMETRY_DISABLED=1
 export OPENSSL_CONF="$HOME/openssl.cnf"
+export NODE_EXTRA_CA_CERTS=~/qlcerts.pem
 
 if [[ -f /etc/ssl/certs/ql-ca-bundle-openssl.crt ]]; then
   export AWS_CA_BUNDLE=/etc/ssl/certs/ql-ca-bundle-openssl.crt
