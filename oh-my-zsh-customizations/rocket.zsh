a16ba53f7f05c86131a336b1604d4ad604590659
Andy Pickler <andy.pickler@gmail.com>
2026-03-11 08:46:00 -0600

    Rocket updates

--- oh-my-zsh-customizations/rocket.zsh
+++ oh-my-zsh-customizations/rocket.zsh
@@ -1,9 +1,12 @@
 # aliases
 
 # exports
-export OPENSSL_CONF="$HOME/openssl.cnf"
-export NODE_EXTRA_CA_CERTS=~/qlcerts.pem
-export AWS_CA_BUNDLE=~/qlcerts.pem
+# Add ability to run Postgres `psql` executable
+export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
+
+# These are now set in ~/.zshrc
+#export NODE_EXTRA_CA_CERTS=~/qlcerts.pem
+#export AWS_CA_BUNDLE=~/qlcerts.pem
 
 # if [[ -f /etc/ssl/certs/ql-ca-bundle-openssl.crt ]]; then
 #   export AWS_CA_BUNDLE=/etc/ssl/certs/ql-ca-bundle-openssl.crt
